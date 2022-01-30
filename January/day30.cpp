2cda4b3ce0a2ada5a6d7c6454dd231d33cfbdbfe
Astel Thottankara <57724542+astonizer@users.noreply.github.com>
2022-01-30 10:21:27 +0530

    add day30 solution

---
+++ January/day30.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    void reverse(vector<int> &nums, int l, int r) {
+        while(l<r) swap(nums[l++], nums[r--]);
+    }
+    
+public:
+    void rotate(vector<int>& nums, int k) {
+        k %= nums.size();
+        reverse(nums, 0, nums.size()-1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.size()-1);
+    }
+};
